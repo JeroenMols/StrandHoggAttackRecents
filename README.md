a523c1d4c6786c6a211f22b3955a52c17496d489
Jeroen Mols <jeroen@jeroenmols.com>
2021-10-14 16:30:57 +0200

    Sample project the reproduces the attack

---
+++ README.md
@@ -0,0 +1,18 @@
+# StrandHogg attack recents
+
+## App under attack
+- `android:launchMode="singleTask"`
+
+## Attacker app
+- `android:launchMode="singleTask"`
+- `android:taskAffinity="com.example.appunderattack"`
+- `android:excludeFromRecents="true"`
+- `moveTaskToBack(true)`
+
+## Reproduction case
+1. Device running Android 10 or lower
+1. Open App under attack via icon
+1. Press Android home button
+1. Open Attacker app (nothing shown)
+1. Open task switcher and select App under attack from recents
+1. Attacker app displays activity on top
---
+++ app-under-attack/src/main/java/com/example/appunderattack/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.appunderattack
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.appunderattack.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
